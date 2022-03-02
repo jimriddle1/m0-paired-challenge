fda565f14d6ff833898110b60f104c57db65294c
Jim Riddle <jimriddle0@gmail.com>
2022-03-01 21:47:49 -0600

    Add 2 files

---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+#3 Book Titles
+Lord of the Flies
+The Stranger
+The Great Gatsby
+Farewell to Arms
+Romeo and Juliet
---
+++ challenge/paird_challenge.rb
@@ -0,0 +1,19 @@
+#I tried typing out 'paired_challenege' but had a typo so it became 'paird_challenge'
+
+#6 variables, strings, ints, booleans
+
+#naming the location as a string - Chicago
+library_location = "Chicago"
+#declaring the number of available books as an interger, setting it to 1000
+library_num_books = 1000
+library_available = true
+#naming another best seller
+library_best_seller = "harry potter"
+library_series_available = 7
+library_is_a_good_book = false
+
+#creating an array with different genres
+library_genres = ["Sci-fi", "Young Adult", "Fantasy", "History"]
+
+
+#
